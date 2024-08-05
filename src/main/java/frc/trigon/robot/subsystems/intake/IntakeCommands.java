5cb8de08284ebf50c53861cf1e0a8a01306b5446
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-08-05 15:03:22 +0300

    fixed intake commands

--- src/main/java/frc/trigon/robot/subsystems/intake/IntakeCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/intake/IntakeCommands.java
@@ -1,28 +1,30 @@
 package frc.trigon.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.trigon.robot.RobotContainer;
 
 public class IntakeCommands {
 
     public static Command getSetTargetStateCommand(IntakeConstants.IntakeState targetState) {
-        return getSetTargetVoltageSequentialCommand(targetState.voltage);
-    }
-
-    public static Command getSetTargetVoltageSequentialCommand(double collectorVoltage) {
-        return new SequentialCommandGroup(
-                getSetTargetVoltageCommand(collectorVoltage).until(RobotContainer.INTAKE::hasNote),
-                getStopIntakeCommand()
+        return new FunctionalCommand(
+                () -> {
+                },
+                () -> RobotContainer.INTAKE.setTargetVoltage(targetState.voltage),
+                (interrupted) -> RobotContainer.INTAKE.stop(),
+                RobotContainer.INTAKE::hasNote,
+                RobotContainer.INTAKE
         );
     }
 
-    private static Command getSetTargetVoltageCommand(double collectorVoltage) {
-        return new StartEndCommand(
-                () -> RobotContainer.INTAKE.setTargetVoltage(collectorVoltage),
+    public static Command getSetTargetVoltageCommand(double collectorVoltage) {
+        return new FunctionalCommand(
                 () -> {
                 },
+                () -> RobotContainer.INTAKE.setTargetVoltage(collectorVoltage),
+                (interrupted) -> RobotContainer.INTAKE.stop(),
+                () -> false,
                 RobotContainer.INTAKE
         );
     }
