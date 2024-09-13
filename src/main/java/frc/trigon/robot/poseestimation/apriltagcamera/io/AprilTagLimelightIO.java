0710984a6cdb72dff5e764262c84f03291455a21
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-09-13 16:17:01 +0300

    Just tell me what I need to change it to, honestly

--- src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/io/AprilTagLimelightIO.java
+++ src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/io/AprilTagLimelightIO.java
@@ -1,5 +1,6 @@
 package frc.trigon.robot.poseestimation.apriltagcamera.io;
 
+import frc.trigon.robot.constants.FieldConstants;
 import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCameraIO;
 import frc.trigon.robot.poseestimation.apriltagcamera.RobotPoseSourceInputsAutoLogged;
 import org.trigon.utilities.LimelightHelpers;
@@ -18,6 +19,31 @@ public class AprilTagLimelightIO extends AprilTagCameraIO {
             final LimelightHelpers.Results results = LimelightHelpers.getLatestResults(hostname).targetingResults;
             inputs.solvePNPPose = results.getBotPose3d_wpiBlue();
             inputs.lastResultTimestamp = results.timestamp_LIMELIGHT_publish;
+            inputs.visibleTagIDs = getVisibleTagIDs(results);
+            inputs.bestTargetRelativeYawRadians = results.getBotPose3d_wpiBlue().getRotation().getZ();
+            inputs.bestTargetRelativePitchRadians = results.getBotPose3d_wpiBlue().getRotation().getY();
+            inputs.averageDistanceFromAllTags = getAverageDistanceFromAllTags(results);
+            inputs.distanceFromBestTag = getDistanceFromBestTag(results);
         }
+    }
+
+    private int[] getVisibleTagIDs(LimelightHelpers.Results results) {
+        LimelightHelpers.LimelightTarget_Fiducial[] visibleTags = results.targets_Fiducials;
+        int[] visibleTagIDs = new int[visibleTags.length];
+        for (int i = 0; i < visibleTagIDs.length; i++)
+            visibleTagIDs[i] = (int) visibleTags[i].fiducialID;
+        return visibleTagIDs;
+    }
+
+    private double getAverageDistanceFromAllTags(LimelightHelpers.Results results) {
+        int[] visibleTags = getVisibleTagIDs(results);
+        double totalDistanceFromTags = 0;
+        for (int i = 0; i < visibleTags.length; i++)
+            totalDistanceFromTags += FieldConstants.TAG_ID_TO_POSE.get(i).getTranslation().getDistance(results.getBotPose3d_wpiBlue().getTranslation());
+        return totalDistanceFromTags /= 4;
+    }
+
+    private double getDistanceFromBestTag(LimelightHelpers.Results results) {
+        return FieldConstants.TAG_ID_TO_POSE.get(getVisibleTagIDs(results)[0]).getTranslation().getDistance(results.getBotPose3d_wpiBlue().getTranslation());
     }
 }
