10655c557f4b6eef5db720db3070a9303d022be1
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-09-15 00:13:17 +0300

    Removed PhotonPoseEstimator

--- src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCameraConstants.java
+++ src/main/java/frc/trigon/robot/poseestimation/apriltagcamera/AprilTagCameraConstants.java
@@ -7,16 +7,12 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import frc.trigon.robot.poseestimation.apriltagcamera.io.AprilTagLimelightIO;
 import frc.trigon.robot.poseestimation.apriltagcamera.io.AprilTagPhotonCameraIO;
-import frc.trigon.robot.poseestimation.photonposeestimator.PhotonPoseEstimator;
 
 import java.util.HashMap;
 import java.util.function.Function;
 
 public class AprilTagCameraConstants {
     public static final HashMap<Integer, Pose3d> TAG_ID_TO_POSE = new HashMap<>();
-    static final PhotonPoseEstimator.PoseStrategy
-            PRIMARY_POSE_STRATEGY = PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-            SECONDARY_POSE_STRATEGY = PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_HEADING;
     static AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
     static {
