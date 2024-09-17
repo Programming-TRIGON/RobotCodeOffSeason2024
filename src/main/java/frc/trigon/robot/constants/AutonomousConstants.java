9a7309dd282494d962cff1aff47f8db376f05038
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-09-17 18:21:33 +0300

    reorginzed FrontRow4NoteAuto

--- src/main/java/frc/trigon/robot/constants/AutonomousConstants.java
+++ src/main/java/frc/trigon/robot/constants/AutonomousConstants.java
@@ -5,6 +5,7 @@ import com.pathplanner.lib.path.PathConstraints;
 import frc.trigon.robot.commands.CommandConstants;
 import frc.trigon.robot.commands.factories.AutonomousCommands;
 import frc.trigon.robot.commands.factories.ShootingCommands;
+import frc.trigon.robot.subsystems.shooter.ShooterCommands;
 
 public class AutonomousConstants {
     public static final PathConstraints REAL_TIME_CONSTRAINTS = new PathConstraints(2.5, 2.5, 4, 4);
@@ -18,6 +19,7 @@ public class AutonomousConstants {
         NamedCommands.registerCommand("AlignToNote", AutonomousCommands.getAlignToNoteCommand());
         NamedCommands.registerCommand("AlignToSpeaker", AutonomousCommands.getAlignToSpeakerCommand());
         NamedCommands.registerCommand("PrepareForShooting", ShootingCommands.getWarmSpeakerShotCommand());
+        NamedCommands.registerCommand("StopShooting", ShooterCommands.getStopCommand());
         NamedCommands.registerCommand("FeedNote", AutonomousCommands.getFeedNoteCommand());
         NamedCommands.registerCommand("CloseShot", ShootingCommands.getPrepareCloseSpeakerShotCommand());
         NamedCommands.registerCommand("Eject", CommandConstants.EJECT_COMMAND);
