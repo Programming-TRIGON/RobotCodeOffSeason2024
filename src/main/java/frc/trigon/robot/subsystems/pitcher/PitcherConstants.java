871c0f949429a39b31b594edf63ec81a810c5ae5
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-10-05 23:38:58 +0300

    pitcher is still overshooting by 2 degrees

--- src/main/java/frc/trigon/robot/subsystems/pitcher/PitcherConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/pitcher/PitcherConstants.java
@@ -20,7 +20,6 @@ import org.trigon.hardware.phoenix6.cancoder.CANcoderSignal;
 import org.trigon.hardware.phoenix6.talonfx.TalonFXMotor;
 import org.trigon.hardware.phoenix6.talonfx.TalonFXSignal;
 import org.trigon.hardware.simulation.SingleJointedArmSimulation;
-import org.trigon.utilities.Conversions;
 import org.trigon.utilities.mechanisms.DoubleJointedArmMechanism2d;
 
 public class PitcherConstants {
@@ -43,15 +42,15 @@ public class PitcherConstants {
     private static final boolean FOLLOWER_OPPOSES_MASTER = true;
     private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Brake;
     private static final double
-            P = RobotHardwareStats.isSimulation() ? 100 : 1,
+            P = RobotHardwareStats.isSimulation() ? 100 : 80,
             I = RobotHardwareStats.isSimulation() ? 0 : 0,
-            D = RobotHardwareStats.isSimulation() ? 20 : 4,
-            KS = RobotHardwareStats.isSimulation() ? 0.2 : 0.18078,
-            KV = RobotHardwareStats.isSimulation() ? 32 : 26.981,
-            KA = RobotHardwareStats.isSimulation() ? 0 : 1.4593,
-            KG = RobotHardwareStats.isSimulation() ? 0.2 : 0.25961;
+            D = RobotHardwareStats.isSimulation() ? 20 : 5,
+            KS = RobotHardwareStats.isSimulation() ? 0.2 : 0.055145,
+            KV = RobotHardwareStats.isSimulation() ? 32 : 30.85,
+            KA = RobotHardwareStats.isSimulation() ? 0 : 0.67839,
+            KG = RobotHardwareStats.isSimulation() ? 0.2 : 0.19735;
     private static final double
-            EXPO_KV = KV,
+            EXPO_KV = KV + 15,
             EXPO_KA = KA;
     private static final GravityTypeValue GRAVITY_TYPE_VALUE = GravityTypeValue.Arm_Cosine;
     private static final StaticFeedforwardSignValue STATIC_FEEDFORWARD_SIGN_VALUE = StaticFeedforwardSignValue.UseVelocitySign;
@@ -61,7 +60,7 @@ public class PitcherConstants {
             REVERSE_SOFT_LIMIT_THRESHOLD = Rotation2d.fromDegrees(13),
             FORWARD_SOFT_LIMIT_THRESHOLD = Rotation2d.fromDegrees(72);
     private static final SensorDirectionValue ENCODER_SENSOR_DIRECTION_VALUE = SensorDirectionValue.Clockwise_Positive;
-    private static final double ENCODER_MAGNET_OFFSET_VALUE = -0.13898 + 0.5 + Conversions.degreesToRotations(90 + 12);
+    private static final double ENCODER_MAGNET_OFFSET_VALUE = -0.34932;
     private static final AbsoluteSensorRangeValue ENCODER_ABSOLUTE_SENSOR_RANGE_VALUE = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
     static final boolean FOC_ENABLED = true;
 
