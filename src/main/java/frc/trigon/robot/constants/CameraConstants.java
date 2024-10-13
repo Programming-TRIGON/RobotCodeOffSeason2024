761632412a34ff5f395e3eee348481274f8788d1
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-10-13 04:50:51 +0300

    Updated camera stuff

--- src/main/java/frc/trigon/robot/constants/CameraConstants.java
+++ src/main/java/frc/trigon/robot/constants/CameraConstants.java
@@ -14,29 +14,29 @@ public class CameraConstants {
             THETA_STD_EXPONENT = 0.01;
 
     private static final Transform3d
-//            FRONT_CENTER_TO_CAMERA = new Transform3d(
-//            new Translation3d(0.0465, 0.325, 0.192),
-//            new Rotation3d(0, Units.degreesToRadians(0), 0)
-//    ),
+            FRONT_CENTER_TO_CAMERA = new Transform3d(
+            new Translation3d(0.0465, 0.325, 0.192),
+            new Rotation3d(0, Units.degreesToRadians(0), 0)
+    ),
             REAR_CENTER_TO_CAMERA = new Transform3d(
-            new Translation3d(-0.325 + 0.00975, 0, 0.095),
-            new Rotation3d(Math.PI - Units.degreesToRadians(0), Units.degreesToRadians(-32.5), Math.PI + Units.degreesToRadians(0))
-    );
+                    new Translation3d(-0.325 + 0.00975, 0, 0.095),
+                    new Rotation3d(Math.PI - Units.degreesToRadians(0), Units.degreesToRadians(-32.5), Math.PI + Units.degreesToRadians(0))
+            );
 
     public static final AprilTagCamera
-//            FRONT_TAG_CAMERA = new AprilTagCamera(
-//            AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
-//            "FrontTagCamera",
-//            FRONT_CENTER_TO_CAMERA,
-//            THETA_STD_EXPONENT,
-//            TRANSLATIONS_STD_EXPONENT
-//    ),
-            REAR_TAG_CAMERA = new AprilTagCamera(
+            FRONT_TAG_CAMERA = new AprilTagCamera(
             AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
-            "RearTagCamera",
-            REAR_CENTER_TO_CAMERA,
+            "FrontTagCamera",
+            FRONT_CENTER_TO_CAMERA,
             THETA_STD_EXPONENT,
             TRANSLATIONS_STD_EXPONENT
-    );
-    public static final ObjectDetectionCamera NOTE_DETECTION_CAMERA = new ObjectDetectionCamera("Collection Camera");
+    ),
+            REAR_TAG_CAMERA = new AprilTagCamera(
+                    AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
+                    "RearTagCamera",
+                    REAR_CENTER_TO_CAMERA,
+                    THETA_STD_EXPONENT,
+                    TRANSLATIONS_STD_EXPONENT
+            );
+    public static final ObjectDetectionCamera NOTE_DETECTION_CAMERA = new ObjectDetectionCamera("NoteDetectionCamera");
 }
