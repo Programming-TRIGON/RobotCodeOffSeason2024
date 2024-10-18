6d281ce65fcfb814b4f8d638c84387575c95d57b
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-10-18 16:01:26 +0300

    Fixed final LED stuff and cleaned `LEDStripCommands`

--- src/main/java/frc/trigon/robot/subsystems/ledstrip/LEDStripCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/ledstrip/LEDStripCommands.java
@@ -2,8 +2,6 @@ package frc.trigon.robot.subsystems.ledstrip;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import org.trigon.commands.ExecuteEndCommand;
 import org.trigon.commands.InitExecuteCommand;
 
@@ -12,33 +10,36 @@ import java.util.function.Supplier;
 
 public class LEDStripCommands {
     public static Command getStaticColorCommand(Color color, LEDStrip... ledStrips) {
-        return new StartEndCommand(
-                () -> {
-                },
-                () -> runForLEDs((ledStrip -> ledStrip.staticColor(color)), ledStrips),
+        return new ExecuteEndCommand(
+                () -> runForLEDs((LEDStrip -> LEDStrip.staticColor(color)), ledStrips),
+                () -> runForLEDs(LEDStrip::clearLedColors, ledStrips),
                 ledStrips
         ).ignoringDisable(true);
     }
 
     public static Command getBlinkingCommand(Color color, double blinkingIntervalSeconds, LEDStrip... ledStrips) {
         return new ExecuteEndCommand(
-                () -> runForLEDs((ledStrip -> ledStrip.blink(color, blinkingIntervalSeconds)), ledStrips),
-                () -> runForLEDs((LEDStrip::clearLedColors)),
+                () -> runForLEDs((LEDStrip -> LEDStrip.blink(color, blinkingIntervalSeconds)), ledStrips),
+                () -> runForLEDs(LEDStrip::clearLedColors, ledStrips),
                 ledStrips
         ).ignoringDisable(true);
     }
 
     public static Command getRainbowCommand(LEDStrip... ledStrips) {
-        return new RunCommand(
+        return new ExecuteEndCommand(
                 () -> runForLEDs((LEDStrip::rainbow), ledStrips),
+                () -> runForLEDs(LEDStrip::rainbow, ledStrips),
                 ledStrips
         ).ignoringDisable(true);
     }
 
     public static Command getBreatheCommand(Color color, int breathingLEDs, double cycleTimeSeconds, boolean shouldLoop, LEDStrip... ledStrips) {
         return new ExecuteEndCommand(
-                () -> runForLEDs((ledStrip) -> ledStrip.breathe(color, breathingLEDs, cycleTimeSeconds, shouldLoop), ledStrips),
-                () -> runForLEDs(LEDStrip::resetBreatheSettings, ledStrips),
+                () -> runForLEDs((LEDStrip) -> LEDStrip.breathe(color, breathingLEDs, cycleTimeSeconds, shouldLoop), ledStrips),
+                () -> {
+                    runForLEDs(LEDStrip::resetBreatheSettings, ledStrips);
+                    runForLEDs(LEDStrip::clearLedColors, ledStrips);
+                },
                 ledStrips
         ).ignoringDisable(true);
     }
@@ -46,13 +47,13 @@ public class LEDStripCommands {
     public static Command getThreeSectionColorCommand(Supplier<Color> firstSectionColor, Supplier<Color> secondSectionColor, Supplier<Color> thirdSectionColor, LEDStrip... ledStrips) {
         return new InitExecuteCommand(
                 () -> runForLEDs(LEDStrip::clearLedColors, ledStrips),
-                () -> runForLEDs((ledStrip) -> ledStrip.threeSectionColor(firstSectionColor.get(), secondSectionColor.get(), thirdSectionColor.get()), ledStrips),
+                () -> runForLEDs((LEDStrip) -> LEDStrip.threeSectionColor(firstSectionColor.get(), secondSectionColor.get(), thirdSectionColor.get()), ledStrips),
                 ledStrips
         ).ignoringDisable(true);
     }
 
     public static void runForLEDs(Consumer<LEDStrip> action, LEDStrip... ledStrips) {
-        for (LEDStrip ledStrip : ledStrips)
-            action.accept(ledStrip);
+        for (LEDStrip LEDStrip : ledStrips)
+            action.accept(LEDStrip);
     }
 }
