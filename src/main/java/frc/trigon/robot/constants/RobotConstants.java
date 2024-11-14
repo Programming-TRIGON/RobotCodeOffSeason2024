7580fab5a135ccb2cd0a6b672b17a1aa0beb5737
Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
2024-11-14 16:32:01 -0500

    Updated TRIGONLib

--- src/main/java/frc/trigon/robot/constants/RobotConstants.java
+++ src/main/java/frc/trigon/robot/constants/RobotConstants.java
@@ -6,14 +6,15 @@ import org.trigon.utilities.FilesHandler;
 
 public class RobotConstants {
     private static final boolean
-            IS_SIMULATION = false,
+            IS_SIMULATION = true,
             IS_REPLAY = false;
+    private static final RobotHardwareStats.ReplayType REPLAY_TYPE = IS_SIMULATION ? RobotHardwareStats.ReplayType.SIMULATION_REPLAY : RobotHardwareStats.ReplayType.REAL_REPLAY;
     private static final double PERIODIC_TIME_SECONDS = 0.02;
-    public static final String CANIVORE_NAME = "SwerveCANivore";
+    public static final String CANIVORE_NAME = "CANivore";
     public static final String LOGGING_PATH = IS_SIMULATION && !Robot.IS_REAL ? FilesHandler.DEPLOY_PATH + "logs/" : "/media/sda1/akitlogs/";
 
     public static void init() {
-        RobotHardwareStats.setCurrentRobotStats(Robot.IS_REAL, IS_SIMULATION, IS_REPLAY);
+        RobotHardwareStats.setCurrentRobotStats(Robot.IS_REAL, IS_REPLAY ? REPLAY_TYPE : RobotHardwareStats.ReplayType.NONE);
         RobotHardwareStats.setPeriodicTimeSeconds(PERIODIC_TIME_SECONDS);
     }
 }
